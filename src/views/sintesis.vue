0bde3fc48fb37319f4f77c331de9926a154f5099
Jhon Jairo Urueta Alvarez <jhon.urueta@misena.edu.co>
2023-11-03 09:48:54 -0500

    Correcciones 1

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -3,8 +3,7 @@
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
-    p Con el diagrama de actividades se termina la descripción de las tres técnicas usadas para la especificación y el análisis de requisitos, como se pudo analizar cada una de las técnicas nos brinda características para lograr representar gráficamente y de una forma clara, los requisitos que un usuario necesita poder cumplir satisfactoriamente con las solicitudes de los clientes y usuarios, por otro lado se pueden usar todas las técnicas en una especificación y análisis de requisitos si se ve la necesidad.
-    p.mb-5 A continuación, se muestra un mapa conceptual con los elementos más importantes desarrollados en este componente.
+    p.mb-5 Diseñar una estrategia de ciberseguridad efectiva es un proceso continuo y evolutivo, se debe tener en cuenta posibles amenazas que pueden surgir y las necesidades cambiantes de los activos de la información que se deseen proteger en la organización, esto es esencial para mantener un entorno digital seguro.
 
     .row.justify-content-center
       .col-lg-10.mb-5
